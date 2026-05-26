d8603c267571b49a4f8fdd3e3c0860e2506b4116
Rifki Immanuel Sinaga <118416978+rifkiimmanuel@users.noreply.github.com>
2026-05-26 16:38:30 +0700

    fix: replace next.config.ts to js for vercel compatibility

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,41 +1,37 @@
-// "use client";
+"use client";
 
-// import { useState } from "react";
-// import Overview from "@/components/Overview";
-// import Experience from "@/components/Experience";
-// import Projects from "@/components/Projects";
-// import Certificate from "@/components/Certificate";
-// import Contact from "@/components/Contact";
-
-// export default function Home() {
-//   const [message, setMessage] = useState("");
-
-//   const getBackendData = async () => {
-//     try {
-//       const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-
-//       const response = await fetch(`${BASE_URL}/`);
-//       const data = await response.json();
-
-//       setMessage(data.message);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   return (
-//     <main className="flex flex-col min-h-screen">
-//       <Overview />
-//       <Experience />
-//       <Projects />
-//       <Certificate />
-//       <Contact />
-
-     
-//     </main>
-//   );
-// }
+import { useState } from "react";
+import Overview from "@/components/Overview";
+import Experience from "@/components/Experience";
+import Projects from "@/components/Projects";
+import Certificate from "@/components/Certificate";
+import Contact from "@/components/Contact";
 
 export default function Home() {
-  return <h1>DEPLOY TEST SUCCESS 🚀</h1>;
+  const [message, setMessage] = useState("");
+
+  const getBackendData = async () => {
+    try {
+      const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+      const response = await fetch(`${BASE_URL}/`);
+      const data = await response.json();
+
+      setMessage(data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <main className="flex flex-col min-h-screen">
+      <Overview />
+      <Experience />
+      <Projects />
+      <Certificate />
+      <Contact />
+
+     
+    </main>
+  );
 }
