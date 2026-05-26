bb75053a5cac148f65be59eadbf8d31919ff51b8
Rifki Immanuel Sinaga <118416978+rifkiimmanuel@users.noreply.github.com>
2026-05-26 16:34:39 +0700

    test minimal page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,37 +1,41 @@
-"use client";
+// "use client";
 
-import { useState } from "react";
-import Overview from "@/components/Overview";
-import Experience from "@/components/Experience";
-import Projects from "@/components/Projects";
-import Certificate from "@/components/Certificate";
-import Contact from "@/components/Contact";
+// import { useState } from "react";
+// import Overview from "@/components/Overview";
+// import Experience from "@/components/Experience";
+// import Projects from "@/components/Projects";
+// import Certificate from "@/components/Certificate";
+// import Contact from "@/components/Contact";
 
-export default function Home() {
-  const [message, setMessage] = useState("");
+// export default function Home() {
+//   const [message, setMessage] = useState("");
 
-  const getBackendData = async () => {
-    try {
-      const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+//   const getBackendData = async () => {
+//     try {
+//       const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
-      const response = await fetch(`${BASE_URL}/`);
-      const data = await response.json();
+//       const response = await fetch(`${BASE_URL}/`);
+//       const data = await response.json();
 
-      setMessage(data.message);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//       setMessage(data.message);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  return (
-    <main className="flex flex-col min-h-screen">
-      <Overview />
-      <Experience />
-      <Projects />
-      <Certificate />
-      <Contact />
+//   return (
+//     <main className="flex flex-col min-h-screen">
+//       <Overview />
+//       <Experience />
+//       <Projects />
+//       <Certificate />
+//       <Contact />
 
      
-    </main>
-  );
+//     </main>
+//   );
+// }
+
+export default function Home() {
+  return <h1>DEPLOY TEST SUCCESS 🚀</h1>;
 }
